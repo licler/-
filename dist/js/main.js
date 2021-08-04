761ed1c67cec41a6c79327f1fd66c95b49e99282
licler <ute3@yandex.ru>
2021-08-04 20:59:22 +0300

    finish info

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,10 +1,12 @@
 $(function(){
 
  $('.slider__items').slick({	
-        arrows: false,
+        arrows: true,
         dots: true,
         fade: true,
-        autoplay: true
+        autoplay: true,
+        prevArrow: '<button type="button" class="slick-btn slick-prev"><img src="img/content/back.svg" alt="images"></button>',
+        nextArrow: '<button type="button" class="slick-btn slick-next"><img src="img/content/next.svg" alt="images"></button>',
  });
 	
 	
