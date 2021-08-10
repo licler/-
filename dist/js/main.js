af43defd9287855763ab3f8ddd5d076cb14ac36c
licler <ute3@yandex.ru>
2021-08-10 22:02:33 +0300

    start catalog

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,5 +1,5 @@
 $(function(){
-
+//слайдер
  $('.slider__items').slick({	
         arrows: true,
         dots: true,
@@ -8,6 +8,13 @@ $(function(){
         prevArrow: '<button type="button" class="slick-btn slick-prev"><img src="img/content/back.svg" alt="images"></button>',
         nextArrow: '<button type="button" class="slick-btn slick-next"><img src="img/content/next.svg" alt="images"></button>',
  });
+
+ // рейтинг
+
+$(".rate-star").rateYo({
+       rating: 5,
+       starWidth: '14px'
+     });
 	
 	
 	
