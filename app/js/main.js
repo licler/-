3ec173f623b7a390a59ee0bc6d3d8da482201e9a
licler <ute3@yandex.ru>
2021-10-09 20:24:21 +0300

    start and finish news

--- app/js/main.js
+++ app/js/main.js
@@ -50,6 +50,17 @@ $('.catalog__filter-list').removeClass('active');
 });
 
 
+// слайдер цитат
+$('.reviews__items').slick({	
+       arrows: true,
+       // autoplay: true,
+       infinite: true,
+       slidesToShow: 2,
+       slidesToScroll: 2,
+       prevArrow: '<button type="button" class="slick-btn slick-prev"><img src="img/content/back.svg" alt="images"></button>',
+       nextArrow: '<button type="button" class="slick-btn slick-next"><img src="img/content/next.svg" alt="images"></button>',
+});
+
 
 
 });
